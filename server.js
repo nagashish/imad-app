dd5056da32e83147d97523ad9d7750870dd4a239
nagashish <30680524+nagashish@users.noreply.github.com>
2017-08-24 00:05:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,15 +5,31 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
-var articleOne ={
-    title:'|Article One|',
-    heading:'Article One',
-    date:'19-Aug-17',
-    content:` 
-            <p>
-              This is the content of my first article.
-            </p>`
+var articles = {
+    'article-one': {
+        title: '|Article One|',
+        heading: 'Article One',
+        date: '19-Aug-17',
+        content:` 
+                <p>
+                  This is the content of my first article.
+                </p>`},
+    'article-two': {
+        title: '|Article Two|',
+        heading: 'Article Two',
+        date: '20-Aug-17',
+        content:` 
+                <p>
+                  This is the content of my second article.
+                </p>`},
+    'article-three': {
+        title: '|Article Three|',
+        heading: 'Article Three',
+        date: '21-Aug-17',
+        content:` 
+                <p>
+                  This is the content of my third article.
+                </p>`}
 };
 
 function createTemplate (data){
@@ -63,16 +79,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res){
-  res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two', function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
-});
-
-app.get('/article-three', function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
+app.get('/:articleName', function (req, res){
+    //articleName == article-one
+    //articles[articleName = {}content object of article one]
+    var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
