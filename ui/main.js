0a7d67a3ac94ae48e73c65948390effd20c20eca
nagashish <30680524+nagashish@users.noreply.github.com>
2017-09-03 17:53:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById("main-text");
 element.innerHTML ="New Value";
 
 //Move the image
-var element = document.getElementById("madi");
+var img = document.getElementById("madi");
 img.onclick = function(){
   img.style.marginLeft ="100px";  
 };
