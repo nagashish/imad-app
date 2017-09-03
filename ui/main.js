9269419fd996075b569124ba49266655078912d6
nagashish <30680524+nagashish@users.noreply.github.com>
2017-09-03 18:15:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,8 +11,8 @@ function moveRight(){
  while(marginLeft<10){ 
      marginLeft = marginLeft + 1;
   img.style.marginLeft = marginLeft + "px";
-     }
+     
 }
 img.onclick = function(){
     var interval = setInterval(moveRight,10);
-};
+}}
