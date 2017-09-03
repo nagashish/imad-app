eda23236dd1680409a134edd0c2a959f42e8b207
nagashish <30680524+nagashish@users.noreply.github.com>
2017-09-03 18:04:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,7 @@ element.innerHTML ="New Value";
 
 //Move the image
 var img = document.getElementById("madi");
+var marginLeft = 0;
 function moveRight(){
   marginLeft = marginLeft + 1;
   img.style.marginLeft = marginLeft + "px";
