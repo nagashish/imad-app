51d19eee2f1a74ab424d44d5afeac28c528349ff
nagashish <30680524+nagashish@users.noreply.github.com>
2017-09-05 17:52:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,10 +7,22 @@ element.innerHTML ="New Value";
 //Move the image
 var img = document.getElementById("madi");
 var marginLeft = 0;
-function moveRight(){
-  marginLeft = marginLeft + 1;
-  img.style.marginLeft = marginLeft + "px";
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,10);
-};
+ var interval; //Declaring variable globally so that i can use it outside the function.
+ 
+    function moveRight(){
+        
+        if(marginLeft<600){
+            marginLeft += 1;
+            img.style.marginLeft =  marginLeft + "px";
+        }
+       else
+          {
+              clearInterval(interval);
+          }
+    }
+
+    img.onclick = function() {
+        
+          interval = setInterval(moveRight, 10);
+          
+    };
