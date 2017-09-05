e73e153f0897dc055f799705c9d4d1dab72447e1
nagashish <30680524+nagashish@users.noreply.github.com>
2017-09-05 19:04:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,19 @@
+//Counter code
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function() {
+  
+  //Make request to counter endpoint
+  
+  //Capture the response and store it in a variable
+  
+  //Render the variable in the correct span
+  counter = counter +1;
+  var span = document.getElementById('count');
+  span.innerHTML =counter.toString();
+};
+
 console.log('Loaded!');
 
 //Move the image
